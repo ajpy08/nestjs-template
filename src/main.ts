8241487048466a5d275574fe0a713df312dd99a1
Angel Javier Puc Yamá <angelpuc08@gmail.com>
2021-09-07 19:12:36 -0500

    change Logger in Server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger();
+
+  await app.listen(port);
+  logger.log(`Server is running in port: ${await app.getUrl()}`);
 }
 bootstrap();
